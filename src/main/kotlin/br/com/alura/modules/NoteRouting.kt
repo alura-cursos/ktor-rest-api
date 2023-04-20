alexfelipe/ceep-web-api-ktor

first sample with ktor

src/main/kotlin/br/com/alura/modules/NoteRouting.kt
@@ -1,0 +1,50 @@
+package br.com.alura.modules
+
+import br.com.alura.models.toNoteResponse
+import br.com.alura.requests.NoteRequest
+import br.com.alura.requests.toNote
+import br.com.alura.services.NoteService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.*
+
+fun Application.configureNoteRouting(
+    service: NoteService
+) {
+    routing {
+        get("/notes") {
+            val response = service.findAll().map {
+                it.toNoteResponse()
+            }
+            call.respond(HttpStatusCode.OK, response)
+        }
+        post("/notes") {
+            val note = call.receive<NoteRequest>().toNote()
+            val response = service.create(note).toNoteResponse()
+            call.respond(HttpStatusCode.Created, response)
+        }
+        get("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            service.findById(id)?.let { note ->
+                val response = note.toNoteResponse()
+                call.respond(HttpStatusCode.OK, response)
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+        put("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            val note = call.receive<NoteRequest>().toNote(id)
+            service.update(note)
+            val response = note.toNoteResponse()
+            call.respond(HttpStatusCode.OK, response)
+        }
+        delete("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            service.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+

src/main/kotlin/br/com/alura/services/NoteService.kt
@@ -1,0 +1,79 @@
+package br.com.alura.services
+
+import br.com.alura.models.Note
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.statements.InsertStatement
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.*
+
+class NoteService(database: Database) {
+
+    private object Notes : Table() {
+        val id = uuid("id")
+        val title = varchar("title", 255)
+        val message = text("message")
+
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    init {
+        transaction(database) {
+            SchemaUtils.create(Notes)
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    suspend fun findAll(): List<Note> = dbQuery {
+        Notes.selectAll()
+            .map { row -> row.toNote() }
+    }
+
+    suspend fun findById(id: UUID): Note? {
+        return dbQuery {
+            Notes.select { Notes.id eq id }
+                .map { row -> row.toNote() }
+                .singleOrNull()
+        }
+    }
+
+    suspend fun create(note: Note): Note = dbQuery {
+        Notes.insert {
+            it[id] = note.id
+            it[title] = note.title
+            it[message] = note.message
+        }.let {
+            Note(
+                id = it[Notes.id],
+                title = it[Notes.title],
+                message = it[Notes.message]
+            )
+        }
+    }
+
+    suspend fun update(note: Note) {
+        dbQuery {
+            Notes.update({ Notes.id eq note.id }) {
+                it[title] = note.title
+                it[message] = note.message
+            }
+        }
+    }
+
+    suspend fun delete(id: UUID) {
+        dbQuery {
+            Notes.deleteWhere { Notes.id.eq(id) }
+        }
+    }
+
+    private fun ResultRow.toNote() = Note(
+        id = this[Notes.id],
+        title = this[Notes.title],
+        message = this[Notes.message]
+    )
+
+}

alura-cursos/ktor-rest-api

src/main/kotlin/br/com/alura/modules/NoteRouting.kt
@@ -1,0 +1,49 @@
+package br.com.alura.modules
+
+import br.com.alura.models.toNoteResponse
+import br.com.alura.requests.NoteRequest
+import br.com.alura.requests.toNote
+import br.com.alura.services.NoteService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.*
+
+fun Application.configureNoteRouting(
+    service: NoteService
+) {
+    routing {
+        get("/notes") {
+            val response = service.findAll().map {
+                it.toNoteResponse()
+            }
+            call.respond(HttpStatusCode.OK, response)
+        }
+        get("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            service.findById(id)?.let { note ->
+                val response = note.toNoteResponse()
+                call.respond(HttpStatusCode.OK, response)
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+        post("/notes") {
+            val note = call.receive<NoteRequest>().toNote()
+            val response = service.save(note).toNoteResponse()
+            call.respond(HttpStatusCode.Created, response)
+        }
+        put("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            val note = call.receive<NoteRequest>().toNote(id)
+            val response = service.save(note).toNoteResponse()
+            call.respond(HttpStatusCode.OK, response)
+        }
+        delete("/notes/{id}") {
+            val id = UUID.fromString(call.parameters["id"])
+            service.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+

src/main/kotlin/br/com/alura/services/NoteService.kt
@@ -1,0 +1,69 @@
+package br.com.alura.services
+
+import br.com.alura.models.Note
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.*
+
+class NoteService(database: Database) {
+
+    private object Notes : Table() {
+        val id = uuid("id")
+        val title = varchar("title", 255)
+        val message = text("message")
+
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    init {
+        transaction(database) {
+            SchemaUtils.create(Notes)
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    suspend fun findAll(): List<Note> = dbQuery {
+        Notes.selectAll()
+            .map { row -> row.toNote() }
+    }
+
+    suspend fun findById(id: UUID): Note? {
+        return dbQuery {
+            Notes.select { Notes.id eq id }
+                .map { row -> row.toNote() }
+                .singleOrNull()
+        }
+    }
+
+    suspend fun save(note: Note): Note = dbQuery {
+        Notes.insertIgnore {
+            it[id] = note.id
+            it[title] = note.title
+            it[message] = note.message
+        }.let {
+            Note(
+                id = it[Notes.id],
+                title = it[Notes.title],
+                message = it[Notes.message]
+            )
+        }
+    }
+
+    suspend fun delete(id: UUID) {
+        dbQuery {
+            Notes.deleteWhere { Notes.id.eq(id) }
+        }
+    }
+
+    private fun ResultRow.toNote() = Note(
+        id = this[Notes.id],
+        title = this[Notes.title],
+        message = this[Notes.message]
+    )
+
+}